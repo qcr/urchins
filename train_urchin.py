70f89aee721e51ccc40737f95fdd8628d3cc65c1
Java Terry <33javalava@gmail.com>
2024-03-01 09:07:56 +1000

    train script update

--- train_urchin.py
+++ train_urchin.py
@@ -9,12 +9,14 @@ model = YOLO('weights/best.pt')
 
 # train the model
 model.train(data='data/urchin.yaml', 
-            epochs=50, 
+            epochs=2500, 
             imgsz=640,
             workers=10,
             cache=True,
             amp=False,
-            batch=-1
+            batch=-1,
+            patience = 250,
+            pretrained = True
             )
 
 print('done')
