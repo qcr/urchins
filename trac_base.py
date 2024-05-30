969fd9e2f4d984a33160445a08294ae5d46d650c
Java Terry <33javalava@gmail.com>
2024-05-30 14:27:45 +1000

    neaten up files for github

--- trac_base.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load an official or custom model
-model = YOLO('weights/X_Urchin_Detector_2024_03_12.pt')  # Load a custom trained model
-
-# Perform tracking with the model
-results = model.track(source="/home/wardlewo/Reggie/data/20240222_urchin_videos_aims/unseen_test/20240222_aims_GX010887.MP4")  # Tracking with default tracker
-#results = model.track(source="https://youtu.be/LNwODJXcvt4", show=True, tracker="bytetrack.yaml")  # Tracking with ByteTrack tracker
-
