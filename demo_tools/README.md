9d744758ded2ccbca34ae9dd5128a9d13de9ffbd
CaptainReginald <Rwardleworth@outlook.com>
2024-05-30 15:45:33 +1000

    Added a read me for Demo Tools

---
+++ demo_tools/README.md
@@ -0,0 +1,27 @@
+# Demo Tools
+
+## dectect_mass_img.py
+
+Tool that takes a folder of images and runs the detector and outputs the images with the detector's annotations 
+No Inputs 
+
+### To Do 
+- Needs to take the input, output and numbers of images as args rather then be hard Coded
+- Takes values for model from a config
+
+## detect_single_img
+Tool that takes an image and runs the detector and outputs the image with the detector's annotations.
+No Inputs 
+
+### To Do 
+- Should be combined with dectect_mass_img.py
+- Takes values for model from a config
+
+## trac_save.py
+
+Tool that takes a video and runs the detector + tracker over it and ouputs a video with the tracking annotation. 
+No Inputs
+
+## To Do
+- Input video and output through args 
+- Takes values for model from a config
---
+++ demo_tools/trac_save.py
@@ -0,0 +1,45 @@
+import os
+import cv2 as cv
+from ultralytics import YOLO
+
+# Load the YOLOv8 model
+model = YOLO('20240312_yolov8x_urchinDetector_best.pt')
+
+# Open the video file
+video_path = "/home/wardlewo/Reggie/data/20240222_urchin_videos_aims/unseen_test/20240222_aims_GX010899.MP4"
+cap = cv.VideoCapture(video_path)
+video_filename = os.path.splitext(os.path.basename(video_path))[0]
+# Define the codec and create VideoWriter object
+codec = cv.VideoWriter_fourcc(*'mp4v')
+output_file = f"{video_filename}_detection_demo.mp4"
+output_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+output_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv.CAP_PROP_FPS))
+out = cv.VideoWriter(output_file, codec, fps, (output_width, output_height))
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True, conf=0.9)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Write the frame with annotations to the output video
+        out.write(annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        #if cv.waitKey(1) & 0xFF == ord("q"):
+           # break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the output video writer
+cap.release()
+out.release()
+cv.destroyAllWindows()
